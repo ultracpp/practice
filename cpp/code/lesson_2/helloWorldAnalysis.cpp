e29329673207f95063b207908b7b35ae9e598bfa
Chauhan Pruthviraj <ultracpp@vk.com>
2023-02-23 16:40:17 +0530

    created file that explains basic parts of C++

---
+++ cpp/code/lesson_2/helloWorldAnalysis.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // includes is the preprocessor directive that includes iostream file
+
+//  main function executes first while you executes C++ program
+
+int main()
+{
+    // write to the console, called standard c out.
+    std::cout << "Simple C++ Program Showing Text String in Console" << std::endl;
+
+    return 0; // return 0 to the operating system
+}
