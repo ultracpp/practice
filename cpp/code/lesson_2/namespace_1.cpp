63589ce898472da09974f811135f3b129e850000
Chauhan Pruthviraj <ultracpp@vk.com>
2023-02-23 17:25:24 +0530

    added file for namespace

---
+++ cpp/code/lesson_2/namespace_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    cout << "we are using std namespace, so invoke cout and endl,  " << endl;
+    cout << "so we can insert text into console by just using cout instead of using std::cout" << endl;
+
+    return 0;
+}
