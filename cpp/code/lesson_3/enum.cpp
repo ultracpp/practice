702d0c0b0bbfe9af1558d285c642cc35b8ea5e6e
Chauhan Pruthviraj <ultracpp@vk.com>
2023-02-23 22:51:14 +0530

    example of enum

---
+++ cpp/code/lesson_3/enum.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+
+    enum numbers
+    {
+        zero,
+        one,
+        two,
+        three,
+        four,
+        five,
+        six,
+        seven,
+        eight,
+        nine,
+        ten,
+        eleven
+    };
+
+    std::cout << "zero is : " << zero << std::endl;
+    std::cout << "C++ is Number : " << one << " Fastet OOP Language " << std::endl;
+    std::cout << "C and C++ Are  " << two << " Sisters" << std::endl;
+    std::cout << "in C++ there are " << three << " Access Specifier in class, public,private and protected" << std::endl;
+
+    std::cout << four << " Pillars of OOP Programming , Abstraction,Inheritance,overloading and Polymorphism " << std::endl;
+
+    std::cout << "Five is : " << five << std::endl;
+    std::cout << "Inte i" << seven << " is too much fast" << std::endl;
+    std::cout << "window" << eight << " Phones were failure" << std::endl;
+    std::cout << "latest version of window is  : " << eleven << std::endl;
+    
+    return 0;
+}
