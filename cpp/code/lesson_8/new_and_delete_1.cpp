fe27da050eeab10f81d6ce2a8fe303fbfe82b7ff
Chauhan Pruthviraj <ultracpp@vk.com>
2023-03-01 18:06:57 +0530

    storing value in array through pointer

---
+++ cpp/code/lesson_8/new_and_delete_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+void memoryRequest()
+{
+
+    unsigned short int userNumber = 0;
+
+    std::cout << "how many integers do you want to allocated an array ? ";
+    std::cin >> userNumber;
+
+    unsigned short int *createMemoryBlocks = new unsigned short int[userNumber];
+
+    for (unsigned short int index = 0; index < userNumber; ++index)
+    {
+        std::cout << "Enter number for  : " << index << " : ";
+        std::cin >> *(createMemoryBlocks + index);
+    }
+
+    std::cout << "============================================" << std::endl;
+    std::cout << "====== Displaying Entered Numbers ==========" << std::endl;
+
+    for (unsigned short int index = 0; index < userNumber; ++index)
+    {
+        std::cout << "[" << index << "] = " << *(createMemoryBlocks + index) << std::endl;
+    }
+};
+
+int main()
+{
+    memoryRequest();
+    return 0;
+}
