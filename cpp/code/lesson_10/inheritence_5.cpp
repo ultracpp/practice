eba773e21569aed9dd250646e9095aa0c3ebea6b
Chauhan Pruthviraj <ultracpp@vk.com>
2023-03-10 15:35:34 +0530

    added example

--- cpp/code/lesson_10/inheritence_5.cpp
+++ cpp/code/lesson_10/inheritence_5.cpp
@@ -4,17 +4,38 @@ using namespace std;
 
 class Cricketer
 {
+private:
+    bool isBowler, isBatsman;
 
 public:
     void whatCanDo()
     {
         std::cout << "Player can play Cricket" << std::endl;
     }
+    void whatCanDo(bool bowler, bool batsman)
+    {
+        isBowler = bowler;
+        isBatsman = batsman;
+
+        if ((isBowler == true) && (isBatsman == false))
+        {
+            std::cout << "Player is Bowler" << std::endl;
+        }
+        else if ((isBowler == false) && (isBatsman == true))
+        {
+            std::cout << "Player is Batsman" << std::endl;
+        }
+        else if ((isBowler == true) && (isBatsman == true))
+        {
+            std::cout << "Player is Allrounder, play with Ball and Bat" << std::endl;
+        }
+    }
 };
 
 class Jadeja : public Cricketer
 {
 public:
+    using Cricketer::whatCanDo;
     void whatCanDo()
     {
         std::cout << "Jadeja is Good Bowler and Batsman, he can play with bat and ball" << std::endl;
@@ -24,32 +45,23 @@ public:
 class Rohit : public Cricketer
 {
 public:
+    using Cricketer::whatCanDo;
     void whatCanDo()
     {
         std::cout << "Rohit sharma is the captain of indian Team, he is known as hitman" << std::endl;
     }
 };
 
-class Player : public Cricketer
-{
-public:
-};
-
-void whatCanDo(Cricketer player)
-{
-    // enter player object to know what he can does;
-    player.whatCanDo();
-}
-
 int main()
 {
     Jadeja jaddu;
-    jaddu.whatCanDo();
+    jaddu.whatCanDo(true, true);
 
     Rohit captain;
-    captain.whatCanDo();
+    captain.whatCanDo(false, true);
 
-    whatCanDo(captain); // passing captain object
+    Cricketer Bumrah;
+    Bumrah.whatCanDo(true, false);
 
     return 0;
 }
